6dd27abf2e551b7d84f88ec47c97ee13687523dc
wuqr <wql789@qq.com>
2021-03-14 17:40:11 +0800

    security-oauth实现资源服务器

---
+++ src/main/java/com/rrong777/web/config/RrrongResourceServerConfig.java
@@ -0,0 +1,12 @@
+package com.rrong777.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ * 资源服务器配置类
+ */
+@Configuration
+@EnableResourceServer
+public class RrrongResourceServerConfig {
+}

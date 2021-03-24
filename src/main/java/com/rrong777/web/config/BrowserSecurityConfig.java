8fcf2c8fe97ba014302ba07d2b59d2887dc3f37d
wuqr <wql789@qq.com>
2021-03-24 23:17:42 +0800

    Update BrowserSecurityConfig.java
    1

--- src/main/java/com/rrong777/web/config/BrowserSecurityConfig.java
+++ src/main/java/com/rrong777/web/config/BrowserSecurityConfig.java
@@ -10,6 +10,7 @@ import com.rrong777.web.session.Rrrong777ExpiredSessionStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -111,6 +112,7 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
                             SecurityConstants.DEFAULT_SESSION_INVALID_URL,
                             "/oauth/authorize")
                             .permitAll()
+                    .antMatchers(HttpMethod.GET,"/user/*").hasRole("ADMIN")
                     .anyRequest()
                     .authenticated()
                 .and()
